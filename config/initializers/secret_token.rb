e34c19604e74c146f0ae4a745acf490f2445bd17
Zadvorniy Cyril <kir594793@mail.ru>
2012-10-29 13:38:50 +0300

    DFAF

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThirdApp::Application.config.secret_token = 'd402b0e43fc3ed2951e78bd5698bc9d1ab11ba1b8f6474ab8d400bc667d1a74095bea73d1624590b0cb0d3c078ddecc4ea6df0be20382159bb747cbe9d60c839'
